c79e747a2bec3d19f94d315956872a70aa7decf6
jan <jodequito@up.edu.ph>
2025-04-14 15:14:08 +0800

    item #1 updated

--- Dequito_B1_answer/dequito_b1.py
+++ Dequito_B1_answer/dequito_b1.py
@@ -1,23 +1,7 @@
-with open("Exam_Table.csv", "r") as file:
-    csvreader = file.readlines()
-csvreader = csvreader[1:]
+import pandas as pd
+df = pd.read_csv("Exam_Table.csv")
 
-interval_30_0 = []
+interval_30_0 = df[df['Interval']=="30-0"] 
+print(interval_30_0)
 
-for row in csvreader:
-    parts = row.strip().split(",")
-    if len(parts) > 1:
-        interval = parts[8].strip()
-        if interval == str("30-0"):
-            interval_30_0.append(row.strip())
-
-with open("b1_output1.csv", "w") as outfile:
-    for item in interval_30_0:
-        outfile.write(item + "\n")
-
-print()
-
-with open("b1_output1.csv", "r") as file:
-    csvreader = file.readlines()
-for row in csvreader:
-    print(row)
+interval_30_0.to_csv("b1_output1.csv", index=False)
