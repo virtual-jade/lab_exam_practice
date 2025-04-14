168be995f46dbddafc1b2003503587ad20c8264b
jan <jodequito@up.edu.ph>
2025-04-14 15:15:22 +0800

    item #2 updated

--- Dequito_B2_answer/dequito_b2.py
+++ Dequito_B2_answer/dequito_b2.py
@@ -4,4 +4,4 @@ df = pd.read_csv('Exam_Table.csv')
 genus_data = df[df['Genus'].str.startswith("St", na=False)] 
 print(genus_data)
 
-genus_data.to_csv("b1_output1_copy.csv", index=False)
+genus_data.to_csv("b2_output1.csv", index=False)
